cd6685fa2b76e54b70af9f10f5303a5706cf0471
Exortix <40566204+Exortix@users.noreply.github.com>
2023-12-28 21:59:07 +0300

    Add setup and inference tasks

--- src/main.cpp
+++ src/main.cpp
@@ -9,32 +9,54 @@ static bool debug_nn = false; // Set this to true to see e.g. features generated
 static bool is_initialised = false;
 uint8_t *snapshot_buf; // points to the output of the capture
 
-
 /* Function definitions ------------------------------------------------------- */
 bool ei_camera_init(void);
 void ei_camera_deinit(void);
 bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf);
 static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr);
 
+/* Task definitions ------------------------------------------------------- */
+void setupTask(void *pvParameters);
+void inferenceTask(void *pvParameters);
+
 void setup()
 {
     // put your setup code here, to run once:
     Serial.begin(115200);
-    // comment out the below line to start inference immediately after upload
+
+    xTaskCreate(
+        setupTask,
+        "SetupTask",
+        10000, // Stack size (bytes)
+        NULL,  // Parameter to pass
+        1,     // Task priority
+        NULL); // Task handle
+
+    vTaskDelete(NULL);
+}
+
+void setupTask(void *pvParameters)
+{
     while (!Serial)
         ;
-    Serial.println("Edge Impulse Inferencing Demo");
-    if (ei_camera_init() == false)
-    {
-        ei_printf("Failed to initialize Camera!\r\n");
-    }
-    else
-    {
-        ei_printf("Camera initialized\r\n");
-    }
 
-    ei_printf("\nStarting continious inference in 2 seconds...\n");
-    ei_sleep(2000);
+    Serial.println("Edge Impulse Inferencing Demo");
+
+    ei_camera_init()
+        ? ei_printf("Camera initialized\r\n")
+        : ei_printf("Failed to initialize Camera!\r\n");
+
+    ei_printf("\nStarting continuous inference in 2 seconds...\n");
+
+    xTaskCreate(
+        inferenceTask,
+        "InferenceTask",
+        10000,
+        NULL,
+        1,
+        NULL);
+
+    vTaskDelete(NULL);
 }
 
 /**
@@ -42,77 +64,88 @@ void setup()
  *
  * @param[in]  debug  Get debug info if true
  */
-void loop()
+void inferenceTask(void *pvParameters)
 {
-
-    // instead of wait_ms, we'll wait on the signal, this allows threads to cancel us...
-    if (ei_sleep(5) != EI_IMPULSE_OK)
+    while (1)
     {
-        return;
-    }
+        // Perform continuous inference here
+        vTaskDelay(pdMS_TO_TICKS(2000));
 
-    snapshot_buf = (uint8_t *)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
+        // instead of wait_ms, we'll wait on the signal, this allows threads to cancel us...
+        if (ei_sleep(5) != EI_IMPULSE_OK)
+        {
+            return;
+        }
 
-    // check if allocation was successful
-    if (snapshot_buf == nullptr)
-    {
-        ei_printf("ERR: Failed to allocate snapshot buffer!\n");
-        return;
-    }
+        // used pvPortMalloc instead of malloc to ensure buffer is allocated in RAM
+        snapshot_buf = (uint8_t *)pvPortMalloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
 
-    ei::signal_t signal;
-    signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
-    signal.get_data = &ei_camera_get_data;
+        // check if allocation was successful
+        if (snapshot_buf == nullptr)
+        {
+            ei_printf("ERR: Failed to allocate snapshot buffer!\n");
+            return;
+        }
 
-    if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false)
-    {
-        ei_printf("Failed to capture image\r\n");
-        free(snapshot_buf);
-        return;
-    }
+        ei::signal_t signal;
+        signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+        signal.get_data = &ei_camera_get_data;
 
-    // Run the classifier
-    ei_impulse_result_t result = {0};
+        if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false)
+        {
+            ei_printf("Failed to capture image\r\n");
+            vPortFree(snapshot_buf); // used vPortFree instead of free to ensure buffer is freed from RAM
+            return;
+        }
 
-    EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
-    if (err != EI_IMPULSE_OK)
-    {
-        ei_printf("ERR: Failed to run classifier (%d)\n", err);
-        return;
-    }
+        // Run the classifier
+        ei_impulse_result_t result = {0};
 
-    // print the predictions
-    ei_printf("predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
-              result.timing.dsp, result.timing.classification, result.timing.anomaly);
+        EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
+        if (err != EI_IMPULSE_OK)
+        {
+            ei_printf("ERR: Failed to run classifier (%d)\n", err);
+            return;
+        }
+
+        // print the predictions
+        ei_printf("predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+                  result.timing.dsp, result.timing.classification, result.timing.anomaly);
 
 #if EI_CLASSIFIER_OBJECT_DETECTION == 1
-    bool bb_found = result.bounding_boxes[0].value > 0;
-    for (size_t ix = 0; ix < result.bounding_boxes_count; ix++)
-    {
-        auto bb = result.bounding_boxes[ix];
-        if (bb.value == 0)
+        bool bb_found = result.bounding_boxes[0].value > 0;
+        for (size_t ix = 0; ix < result.bounding_boxes_count; ix++)
         {
-            continue;
+            auto bb = result.bounding_boxes[ix];
+            if (bb.value == 0)
+            {
+                continue;
+            }
+            ei_printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
         }
-        ei_printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
-    }
-    if (!bb_found)
-    {
-        ei_printf("    No objects found\n");
-    }
+        if (!bb_found)
+        {
+            ei_printf("    No objects found\n");
+        }
 #else
-    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++)
-    {
-        ei_printf("    %s: %.5f\n", result.classification[ix].label,
-                  result.classification[ix].value);
-    }
+        for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++)
+        {
+            ei_printf("    %s: %.5f\n", result.classification[ix].label,
+                      result.classification[ix].value);
+        }
 #endif
 
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
-    ei_printf("    anomaly score: %.3f\n", result.anomaly);
+        ei_printf("    anomaly score: %.3f\n", result.anomaly);
 #endif
 
-    free(snapshot_buf);
+        vPortFree(snapshot_buf); // used vPortFree instead of free to ensure buffer is freed from RAM
+    }
+}
+
+void loop()
+{
+    // nothing to do here
 }
 
 /**
